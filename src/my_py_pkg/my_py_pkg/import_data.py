25b6ee2f672d95f952b4e3e46fdc0a57e4d5cd8b
Illera1999 <illerarodriguez1999@gmail.com>
2023-11-13 17:57:48 +0000

    Vertical shoulder angle, limited between 0 and 180

--- src/my_py_pkg/my_py_pkg/import_data.py
+++ src/my_py_pkg/my_py_pkg/import_data.py
@@ -6,40 +6,31 @@ import math
 from rclpy.node import Node
 from my_cpp_interfaces.msg import DataRight
 
-#rtde_c = rtde_control.RTDEControlInterface("192.168.0.100")
+rtde_c = rtde_control.RTDEControlInterface("192.168.0.100")
+pi = math.pi
 
 class BoneData:
-    def __init__(self, name, position):
-        #self.RightShoulder = [name[0], position[0], position[1], position[2]]
-        self.RightArm = [name[1], position[3], position[4], position[5]]
-        self.RightForeArm = [name[2], position[6], position[7],  position[8]]
-        self.RightHandº = [name[3], position[9], position[10], position[11]]
+    def __init__(self, name, position, quaternio):
+        self.RightShoulder = [name[0], position[0], position[1], position[2], quaternio[0], quaternio[1], quaternio[2], quaternio[3]]
+        self.RightShoulderVakue = [name[0], position[0] - position[0], position[1] - position[1], position[2] - position[2]]
+
+        self.RightArm = [name[1], position[3], position[4], position[5], quaternio[4], quaternio[5], quaternio[6], quaternio[7]]
+        self.RightArmValue = [name[1], position[3] - position[0], position[4] - position[1], position[5] - position[2], self.RightArm[4]]
+        self.RightForeArm = [name[2], position[6], position[7],  position[8], quaternio[8], quaternio[9], quaternio[10], quaternio[11]]
+        self.RightHand = [name[3], position[9], position[10], position[11], quaternio[12], quaternio[13], quaternio[14], quaternio[15]]
         self.name = name
-        self.position = position
+        self.position = position        
 
     def printData(self):
-        #self.printauxData(self.RightShoulder)    
-        self.printauxData(self.RightArm, self.position, self.name)    
-        self.printauxData(self.RightForeArm, self.position, self.name)    
-        self.printauxData(self.RightHando, self.position, self.name)    
-
-    def printauxData(self, datos, position, name):
-        self.RightArmValue = [name[1], position[3] - position[0], position[4] - position[1], position[5] - position[2]]
-        self.RightForeArmValue = [name[2], position[6] - position[0], position[7] - position[1], position[8] - position[2]]
-        #self.RightForeArmValueValue = [self.RightForeArmValue[0], self.RightForeArmValue[1] - self.RightArmValue[1], self.RightForeArmValue[2] - self.RightArmValue[2], self.RightForeArmValue[3] - self.RightArmValue[3]]
-        self.RightHandValue = [name[3], position[9] - position[0], position[10] - position[1], position[11] - position[2]]
-
         print("------------------------------")
         print("Datos del brazo: ")
-        print("Nombre: " + datos[0])
-        print("Posición: { " + 
-            str(datos[1]) + ", " + 
-            str(datos[2]) + ", " + 
-            str(datos[3]) + " }")
-        print("Posición: { " + 
-            str(self.RightArmValue[1]) + ", " + 
-            str(self.RightForeArm[2]) + ", " + 
-            str(self.RightHandValue[3]) + " }")
+
+        print(str(self.RightArm))
+        print(str(self.RightArmValue))
+
+        # print(str(self.RightArmValue))
+        # print(str(self.RightForeArm))
+        # print(str(self.RightHand))
         print("------------------------------")
 
 class ImportData(Node): 
@@ -50,52 +41,56 @@ class ImportData(Node):
         self.get_logger().info("ImportData has been started.")
         
     def callback_data(self,msg):
-        rightArm = BoneData(msg.name, msg.position)
+        rightArm = BoneData(msg.name, msg.position, msg.quaternio)
         rightArm.printData()
-        #base_vertical, base_horizontal = calcular_angulo_base(rightArm.RightArmValue, vertical=False, horizontal=False)
-        #ante_brazo_vertical, ante_brazo_horizontal = calcular_angulo_base(rightArm.RightForeArmValue, vertical=True, horizontal=True)
-        #rtde_c.moveJ([-base_horizontal, -base_vertical, -0, -1.5708, 1.5708, 0], 1, 1)
-        #rtde_c.moveJ([-base_horizontal, -base_vertical, -0, -1.5708, 1.5708, 0], 1, 1)
+        base, hombro = calcular_angulo_base_hombro(rightArm.RightArmValue)
+
+        # rtde_c.moveJ([1.5708, hombro, -0, -1.5708, 1.5708, 0], 1.5 , 1.5)
+        # rtde_c.moveJ([base, -1.5708, -0, -1.5708, 1.5708, 0], 1.5, 1.5)
+        # rtde_c.moveJ([base, hombro, -0, -1.5708, 1.5708, 0], 2, 2)
         #rtde_c.moveJ([1.5708, -base_vertical, -0, -1.5708, 1.5708, 0], 1, 1)
         #rtde_c.moveJ([1.5708, 0, ante_brazo_horizontal, -1.5708, 1.5708, 0], 1, 1)
 
-# def cal_matriz_translate():
-#     return 0
-
-def calcular_angulo_base(datos, vertical = False, horizontal = False):
+def calcular_angulo_base_hombro(datos):
     x = datos[1]
     y = datos[2]
     z = datos[3]
-    # Calcular el ángulo vertical en radianes con respecto al eje Z
-    angulo_vertical_radianes = math.atan2(math.sqrt(x**2 + y**2), z)
-    #angulo_horizontal_radianes = math.atan2(math.sqrt(x**2 + z**2), y)
-    angulo_horizontal_radianes = math.atan2(y,x)
+    quaternio = datos[4]
 
-    # Convertir el ángulo de radianes a grados
-    angulo_vertical_grados = math.degrees(angulo_vertical_radianes)
-    # Convertir el ángulo horizontal de radianes a grados
-    angulo_horizontal_grados = math.degrees(angulo_horizontal_radianes)
+    # -----------------------     Calcula el ángulo entre el vector y el eje x,z usando atan2 ---------------------------------
+    angulo_radianes_horizontal = 0
 
-    if horizontal == True:
-        print("Angulo de la base_horizontal")
-        print(f"Ángulo horizontal en radianes: {angulo_horizontal_radianes}")
-        print(f"Ángulo horizontal en grados: {angulo_horizontal_grados}")
-    if vertical == True:
-        print("Angulo de la base_vertical")
-        print(f"Ángulo vertical en radianes: {angulo_vertical_radianes}")
-        print(f"Ángulo vertical en grados: {angulo_vertical_grados}")
-    return angulo_vertical_radianes, angulo_horizontal_radianes
+    # -----------------------     Calcula el angulo formado por el plano y,z ---------------------------------
+
+    angulo_radianes_vertical = math.atan2(y,z)
+
+    if angulo_radianes_vertical > 0 and quaternio <= 0:
+        angulo_radianes_vertical = 0
+    if angulo_radianes_vertical > 0 and quaternio > 0:
+        angulo_radianes_vertical = -pi
+    
+    
+    angulo_grados_vertical = math.degrees(angulo_radianes_vertical)
+
+
+    print("--------------------------------------")
+    print("Posicion:" + str(datos))
+    print("Angulo vertical hombro:")
+    print(str(int(angulo_grados_vertical)) + "º \n")
+    # print("Angulo horizontal hombro:")
+    # print(str(int(angulo_grados_horizontal)) + "º \n")
+
+    return angulo_radianes_horizontal, angulo_radianes_vertical
 
 def main(args=None):
-    #rtde_c.moveJ([1.5708, 0, 0, -1.5708, 1.5708, 0], 1, 1)
+    rtde_c.moveJ([1.5708, 0, 0, -1.5708, 1.5708, 0], 1, 1)
     time.sleep(2)
     rclpy.init(args=args)
     node = ImportData() 
     rclpy.spin(node)
     rclpy.shutdown()
     rtde_c.stopJ()
-
-
+    return 0
 
 if __name__ == "__main__":
     main()
--- src/my_py_pkg/my_py_pkg/prueba.py
+++ src/my_py_pkg/my_py_pkg/prueba.py
@@ -1,25 +1,92 @@
-import rtde_control
+import numpy as np
 
-rtde_c = rtde_control.RTDEControlInterface("192.168.0.102")
+def angulo_recta_plano(punto_recta, cuaternion_director):
+  """
+  Calcula el ángulo que forma una recta con respecto a un plano.
 
-# Parameters
-velocity = 0.5
-acceleration = 0.5
-dt = 1.0/500  # 2ms
-lookahead_time = 0.1
-gain = 300
-joint_q = [-1.54, -1.83, -2.28, -0.59, 1.60, 0.023]
+  Args:
+    punto_recta: Punto de la recta.
+    cuaternion_director: Cuaternio que representa el vector director de la recta.
 
-# Move to initial joint position with a regular moveJ
-rtde_c.moveJ(joint_q)
+  Returns:
+    Ángulo en grados.
+  """
 
-# Execute 500Hz control loop for 2 seconds, each cycle is 2ms
-for i in range(1000):
-    t_start = rtde_c.initPeriod()
-    rtde_c.servoJ(joint_q, velocity, acceleration, dt, lookahead_time, gain)
-    joint_q[0] += 0.001
-    joint_q[1] += 0.001
-    rtde_c.waitPeriod(t_start)
+  # Normalizamos el vector normal al plano.
+  vector_normal = np.array([0, 0, 1])
+  vector_normal = vector_normal / np.linalg.norm(vector_normal)
 
-rtde_c.servoStop()
-rtde_c.stopScript()
+  # Obtenemos la parte imaginaria del cuaternion que representa la recta.
+  parte_imaginaria_recta = cuaternion_director.imag
+
+  # Agregamos una dimensión adicional al punto_recta.
+  punto_recta_expandido = np.expand_dims(punto_recta, axis=0)
+
+  # Eliminamos la dimensión adicional del punto_recta_expandido.
+  punto_recta_comprimido = np.squeeze(punto_recta_expandido)
+
+  # Transmitimos el punto_recta a una forma de (4,).
+  punto_recta_transmitido = np.broadcast_to(punto_recta_comprimido, (4,))
+
+  # Obtenemos el vector director como la diferencia entre la parte imaginaria del cuaternion que representa el punto y la parte imaginaria del cuaternion que representa la recta.
+  vector_director = parte_imaginaria_recta - punto_recta_transmitido
+
+  # Calculamos el producto escalar entre el vector normal al plano y el vector director de la recta.
+  producto_escalar = np.dot(vector_normal, vector_director)
+
+  # Calculamos el ángulo entre dos vectores a partir de su producto escalar.
+  angulo = np.arccos(producto_escalar)
+
+  # Convertimos el ángulo de radianes a grados.
+  angulo = angulo * 180 / np.pi
+
+  return angulo
+
+
+# Ejemplo
+punto_recta = [1, 2, 3]
+cuaternion_director = np.array([0, 0, 1, 1])
+
+angulo = angulo_recta_plano(punto_recta, cuaternion_director)
+
+print(angulo)
+
+
+
+
+
+
+
+# import math
+
+# pi=math.pi
+
+# def calcular_angulo_alfa_y_eje(datos):
+#     x = datos[0]
+#     y = datos[1]
+#     z = datos[2]
+#     angulo_radianes_vertical = math.atan2(y,z)
+
+#     angulo_radianes_vertical += pi/2
+
+#     if angulo_radianes_vertical < 0:
+#         angulo_radianes_vertical = 0
+#     elif angulo_radianes_vertical > pi:
+#         angulo_radianes_vertical = pi
+
+#     angulo_grados_vertical = math.degrees(angulo_radianes_vertical)
+
+
+#     print("--------------------------------------")
+#     print("Posicion:" + str(datos))
+#     print("Angulo vertical hombro:")
+#     print(str(int(angulo_grados_vertical)) + "º \n")
+
+
+
+# # Ejemplo de uso
+# x = 1.0
+# y = 1.0
+# z = 1.0
+# # calcular_angulo_alfa_y_eje((x, y, z))
+# calcular_angulo_alfa_y_eje((x, y, z))
