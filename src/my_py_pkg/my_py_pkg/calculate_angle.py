938d9c0c1df367ba09d47cd0c5612411a738a374
Illera1999 <illerarodriguez1999@gmail.com>
2023-11-29 19:28:34 +0000

    Calculate angle of arm and forearm, with position and quaternio

--- src/my_py_pkg/my_py_pkg/calculate_angle.py
+++ src/my_py_pkg/my_py_pkg/calculate_angle.py
@@ -1,79 +1,97 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-pi = np.pi
+def quaternion_to_rotation_matrix(quaternion):
+    """
+    Convert a quaternion into a rotation matrix.
+    """
+    q0, q1, q2, q3 = quaternion
+    R = np.array([
+        [1 - 2*q2**2 - 2*q3**2, 2*q1*q2 - 2*q3*q0, 2*q1*q3 + 2*q2*q0],
+        [2*q1*q2 + 2*q3*q0, 1 - 2*q1**2 - 2*q3**2, 2*q2*q3 - 2*q1*q0],
+        [2*q1*q3 - 2*q2*q0, 2*q2*q3 + 2*q1*q0, 1 - 2*q1**2 - 2*q2**2]
+    ])
+    return R
 
-def base_angel(punto_objetivo):
-    vector_referencia = np.array([0, 0, -1])  # Dirección de referencia (eje Z)
+def calculate_elbow_angle(arm_quat, arm_pos, forearm_quat, forearm_pos):
+    """
+    Calculate the elbow angle from arm and forearm quaternion and position.
+    """
+    # Convert quaternions to rotation matrices
+    arm_rot_matrix = quaternion_to_rotation_matrix(arm_quat)
+    forearm_rot_matrix = quaternion_to_rotation_matrix(forearm_quat)
 
-    # Normalizar los vectores para asegurarse de que tengan magnitudes de 1
-    vector_normalizado = punto_objetivo / np.linalg.norm(punto_objetivo)
-    vector_referencia_normalizado = vector_referencia / np.linalg.norm(vector_referencia)
+    # Define unit vectors for arm and forearm in their local coordinate systems
+    arm_direction = np.array([1, 0, 0])  # Assuming the sensor's X-axis points along the arm
+    forearm_direction = np.array([1, 0, 0])  # Assuming the sensor's X-axis points along the forearm
 
-    # Calcular el producto punto entre los dos vectores
-    producto_punto = np.dot(vector_normalizado, vector_referencia_normalizado)
+    # Transform these vectors to the global coordinate system
+    arm_vector = np.dot(arm_rot_matrix, arm_direction)
+    forearm_vector = np.dot(forearm_rot_matrix, forearm_direction)
 
-    # Calcular el ángulo entre los vectores usando la función inversa del coseno (arccos)
-    angulo_apertura_radianes = np.arccos(producto_punto)
+    # Calculate the angle between the vectors
+    dot_product = np.dot(arm_vector, forearm_vector)
+    angle = np.arccos(dot_product / (np.linalg.norm(arm_vector) * np.linalg.norm(forearm_vector)))
 
-    angulo_apertura_radianes += 1.5708
+    # Convert angle to degrees
+    angle_degrees = np.degrees(angle)
 
-    if (punto_objetivo[1] < 0):
-        angulo_apertura_radianes -= pi
-        if (punto_objetivo[2] < 0):
-            angulo_apertura_radianes = -angulo_apertura_radianes
-        if (punto_objetivo[2] > 0):
-            angulo_apertura_radianes = -angulo_apertura_radianes
+    return angle_degrees, arm_vector, forearm_vector, arm_pos, forearm_pos
 
-    # Convertir el ángulo a grados y ajustamos angulos.
-    # if angulo_apertura_radianes > (3/2)*pi:
-    #     angulo_apertura_radianes = (3/2)*pi
-    # if angulo_apertura_radianes < -(1/2)*pi:
-    #     angulo_apertura_radianes = -(1/2)*pi
+def plot_arm_and_forearm(arm_pos, arm_vector, forearm_pos, forearm_vector, angle):
+    """
+    Create a 3D plot to visualize the arm and forearm position and orientation.
+    """
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
 
-    angulo_apertura_grados = np.degrees(angulo_apertura_radianes)
+    # Plotting the arm and forearm vectors
+    ax.quiver(*arm_pos, *arm_vector, color='blue', length=0.2, label='Arm Direction')
+    ax.quiver(*forearm_pos, *forearm_vector, color='red', length=0.2, label='Forearm Direction')
 
-    print(f"Punto objetivo: {punto_objetivo}")
-    print(f"El ángulo de apertura es: {angulo_apertura_grados} grados")
+    # Plotting the positions of the sensors
+    ax.scatter(*arm_pos, color='blue', s=50, label='Arm Position')
+    ax.scatter(*forearm_pos, color='red', s=50, label='Forearm Position')
 
-    return angulo_apertura_radianes
+    # Setting labels and title
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_zlabel('Z axis')
+    ax.set_title(f'Arm and Forearm Orientation (Elbow Angle: {angle:.2f} degrees)')
 
+    # Setting limits for better visualization
+    ax.set_xlim([-1, 1])
+    ax.set_ylim([-1, 1])
+    ax.set_zlim([-1, 1])
 
+    # Adding a legend
+    ax.legend()
 
-def angulo_entre_lineas(normal, recta):
-    # Calcular el producto escalar y las magnitudes
-    dot_product = np.dot(normal, recta)
-    magnitude1 = np.linalg.norm(normal)
-    magnitude2 = np.linalg.norm(recta)
-
-    # Calcular el ángulo en radianes
-    alpha = np.arccos(dot_product / (magnitude1 * magnitude2))
-
-    alpha -= 1.5708
-    alpha = -alpha
-
-    if recta[0] > 0:
-        alpha = 0
-        alpha_degrees = 0
-
-    # Convertir el ángulo a grados si es necesario
-    alpha_degrees = np.degrees(alpha)
-    if alpha_degrees > 90:
-        alpha = (1/2)*pi 
-        alpha_degrees = 90
-    
-    print(f"El ángulo para el hombro es: {alpha_degrees} grados")
-    
-    return alpha
-
-def hombro_angle(punto):
-    # Vector normal al plano (ejemplo: a lo largo de x)
-    vector_normal = np.array([-1, 0, 0])
-    vector_recta = punto
-
-    print(f"Punto objetivo: {punto}")
-    angle = angulo_entre_lineas(vector_normal,vector_recta)
-    return angle
+    plt.show()
 
 
-# angulo = base_angel((0, -0.25, 1))
-# angulo = hombro_angle((-1,0,0))
+
+# Using the provided data
+# arm_quat = [0.88207424, 0.34301504, 0.06129225, 0.31710738]
+# arm_pos = [0.1452632, 0.26766157, -0.5298454]
+# forearm_quat = [0.71359897, 0.63619936, 0.2074767, 0.2073844]
+# forearm_pos = [0.36693183, 0.40157384, -0.5065659]
+
+# arm_quat = [0.876584, 0.295584, 0.0655076, 0.374123]
+# arm_pos = [0.127087, 0.294738, -0.527362]
+# forearm_quat = [0.761293, -0.0236154, 0.647292, 0.0302769]
+# forearm_pos = [0.182314, 0.329937, -0.779002]
+
+arm_quat = [0.920419, -0.0256769, -0.0707999, 0.383646]
+arm_pos = [0.114518, 0.304706, -0.505085]
+forearm_quat = [0.896905, 0.0451076, -0.0408769, -0.438046]
+forearm_pos = [0.29413, 0.118875, -0.476491]
+
+angle, arm_vector, forearm_vector, arm_pos, forearm_pos = calculate_elbow_angle(arm_quat, arm_pos, forearm_quat, forearm_pos)
+
+# Plot the arm and forearm orientation
+plot_arm_and_forearm(arm_pos, arm_vector, forearm_pos, forearm_vector, angle)
+
+
+
--- src/my_py_pkg/my_py_pkg/import_dataBVH.py
+++ src/my_py_pkg/my_py_pkg/import_dataBVH.py
@@ -11,20 +11,29 @@ from my_py_pkg import combination_angle
 last_angle = True
 
 class BoneData:
-    def __init__(self, name, rotation):
-        self.RightArm = [name[0], rotation[0], rotation[1], rotation[2]]
-        self.RightForeArm = [name[1], rotation[3], rotation[4], rotation[5]]
-        self.RightHand = [name[2], rotation[6], rotation[7], rotation[8]]
+    def __init__(self, name, rotation, quaternio, position):
+        self.RightArm = [name[0], rotation[0], rotation[1], rotation[2], quaternio[0], quaternio[1], quaternio[2], quaternio[3]]
+        self.RightForeArm = [name[1], rotation[3], rotation[4], rotation[5], quaternio[4], quaternio[5], quaternio[6], quaternio[7]]
+        self.RightHand = [name[2], rotation[6], rotation[7], rotation[8], quaternio[8], quaternio[9], quaternio[10], quaternio[11]]
+        self.RightArmPosition = [position[0], position[1], position[2]]
+        self.RightForeArmPosition = [position[3], position[4], position[5]]
+        self.RightHandPosition = [position[6], position[7], position[8]]
         self.name = name
 
     def printData(self):
         print("------------------------------")
         print("Datos del Brazo: ")
         print(str(self.RightArm))
+        print("           Position del Brazo: ")
+        print(str(self.RightArmPosition))
         print("Datos del Ante brazo: ")
         print(str(self.RightForeArm))
+        print("           Position del AnteBrazo: ")
+        print(str(self.RightForeArmPosition))
         print("Datos del Mano: ")
         print(str(self.RightHand))
+        print("           Position del Mano: ")
+        print(str(self.RightHandPosition))
         print("------------------------------")
     
     def hombro_angle(self): #El hombro recorre desde -0 a -90
@@ -58,16 +67,13 @@ class BoneData:
         return angulo
     
     def codo_angle(self):
-        recta1 = combination_angle.obtener_direccion_recta(self.RightArm[1], self.RightArm[2], self.RightArm[3])
-        recta2 = combination_angle.obtener_direccion_recta(self.RightForeArm[1], self.RightForeArm[2], self.RightForeArm[3])
-        angulo = combination_angle.calcular_angulo_entre_rectas(recta1,recta2)
-        angulo -= 90
-        if angulo > 0:
+        angulo = self.RightForeArm[1]
+        if angulo < 0:
             angulo = 0
-        if angulo < -135:
-            angulo = -135
+        if angulo > 90:
+            angulo = 90
         print("Angulo codo " + str(angulo))
-        angulo = np.radians(angulo)
+        angulo = np.radians(-angulo)
         return angulo
 
     def muñeca1_angle(self):
@@ -75,26 +81,28 @@ class BoneData:
         angulo -= 90
         if angulo < -180:
             angulo = -180
-        if angulo > 0:
-            angulo = 0
+        if angulo > -80:
+            angulo = -80
         print("Angulo muñeca_1 " + str(angulo))
         angulo = np.radians(angulo)
         return angulo
     
     def muñeca2_angle(self):
-        angulo = self.RightHand[1]
-        angulo -= 90
-        if angulo < -180:
-            angulo = -180
+        angulo = self.RightForeArm[2]
+        angulo = -angulo
+        angulo -= 50
         if angulo > 0:
             angulo = 0
+        if angulo < -180:
+            angulo = -180
+
         print("Angulo muñeca_2 " + str(angulo))
         angulo = np.radians(angulo)
         return angulo
     
     def muñeca3_angle(self):
-        angulo = self.RightHand[2]
-        print("Angulo muñeca_1 " + str(angulo))
+        angulo = self.RightHand[1]
+        print("Angulo muñeca_3 " + str(angulo))
         angulo = np.radians(angulo)
         return angulo
     
@@ -106,28 +114,27 @@ class ImportData(Node):
         self.get_logger().info("ImportData has been started.")
         
     def callback_data(self,msg):
-        rightArm = BoneData(msg.name, msg.rotation)
+        rightArm = BoneData(msg.name, msg.rotation, msg.quaternio, msg. position)
         rightArm.printData()
-        rightArm.muñeca1_angle()
 
         # rtde_c.moveJ([ 1.5708, rightArm.hombro_angle(), 0, -1.5708, -1.5708, 0], 1.5, 1.5)
         # rtde_c.moveJ([ rightArm.base_angle(), 0, 0, -1.5708, -1.5708, 0], 1.5, 1.5)
         # rtde_c.moveJ([1.5708, 0, rightArm.codo_angle(), -1.5708, -1.5708, 0], 1.5, 1.5)
         # rtde_c.moveJ([rightArm.base_angle(), rightArm.hombro_angle(), rightArm.codo_angle(), -1.5708, -1.5708, 0], 1.5, 1.5)
         # rtde_c.moveJ([1.5708, 0, codo, -1.5708, 1.5708, 0], 1.5, 1.5)
-        rtde_c.moveJ([rightArm.base_angle(), rightArm.hombro_angle(), rightArm.codo_angle(),
-                       rightArm.muñeca1_angle(), rightArm.muñeca2_angle(), rightArm.muñeca3_angle()], 1.5, 1.5)
+        # rtde_c.moveJ([rightArm.base_angle(), rightArm.hombro_angle(), rightArm.codo_angle(),
+        #                rightArm.muñeca1_angle(), rightArm.muñeca2_angle(), rightArm.muñeca3_angle()], 1.5, 1.5)
 
-rtde_c = rtde_control.RTDEControlInterface("192.168.0.100")
+# rtde_c = rtde_control.RTDEControlInterface("192.168.0.100")
 
 
 def main(args=None):
-    rtde_c.moveJ([1.5708, 0, 0, -1.5708, -1.5708, 0], 1, 1)
+    # rtde_c.moveJ([1.5708, 0, 0, -1.5708, -1.5708, 0], 1, 1)
     rclpy.init(args=args)
     node = ImportData() 
     rclpy.spin(node)
     rclpy.shutdown()
-    rtde_c.stopJ()
+    # rtde_c.stopJ()
 
     return 0
 
